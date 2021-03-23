72eb7c45dcf0e18760ed4d781b1abef0037da3e5
Zignake <laukik28avhad@gmail.com>
2021-03-23 14:23:32 +0530

    added makeRequest file

--- esp8266_code/esp8266_code.ino
+++ esp8266_code/esp8266_code.ino
@@ -6,68 +6,18 @@
 const char *ssid = "00000000 2.4";
 const char *password = "ballerina";
 
+String makeRequest();
+String payload;
+
 void setup()
 {
-
   Serial.begin(115200);
-  // Serial.setDebugOutput(true);
-
   WiFi.begin(ssid, password);
 }
 
 void loop()
 {
-  // wait for WiFi connection
-
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    Serial.println("Wifi Connected Success!");
-    Serial.print("NodeMCU IP Address : ");
-    Serial.println(WiFi.localIP());
-
-    WiFiClient client;
-    HTTPClient http;
-    HTTPClient httpMakeRequest;
-
-    httpMakeRequest.begin(client, "http://keluke.000webhostapp.com/from_micro.php?unit=1&sensor=140");
-    int httpMRCode = httpMakeRequest.GET();
-    Serial.printf("httpMRCode: %d\n", httpMRCode);
-
-    Serial.print("[HTTP] begin...\n");
-    if (http.begin(client, "http://keluke.000webhostapp.com/interface.php"))
-    { // client is what type of network are u using. wifi, ethernet, gsm.
-
-      Serial.print("[HTTP] GET...\n");
-      // start connection and send HTTP header
-      int httpCode = http.GET();
-
-      // httpCode will be negative on error
-      if (httpCode > 0)
-      {
-        // HTTP header has been sent and Server response header has been handled
-        Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-
-        // file found at server
-        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY)
-        {
-          String payload = http.getString();
-          Serial.println("start");
-          Serial.println(payload);
-          Serial.println("end");
-        }
-      }
-      else
-      {
-        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-      }
-
-      http.end();
-    }
-    else
-    {
-      Serial.printf("[HTTP} Unable to connect\n");
-    }
-  }
-
-  delay(10000);
+  delay(5000);
+  payload = makeRequest();
+  Serial.println(payload); 
 }
---
+++ esp8266_code/makeRequest.ino
@@ -0,0 +1,53 @@
+String makeRequest()
+{
+  if (WiFi.status() == WL_CONNECTED)
+  {
+//    Serial.println("Wifi Connected Success!");
+//    Serial.print("NodeMCU IP Address : ");
+//    Serial.println(WiFi.localIP());
+
+    WiFiClient client;
+    HTTPClient http;
+    HTTPClient httpMakeRequest;
+    String payload;
+
+    httpMakeRequest.begin(client, "http://keluke.000webhostapp.com/from_micro.php?unit=1&sensor=140");
+    int httpMRCode = httpMakeRequest.GET();
+    Serial.printf("httpMRCode: %d\n", httpMRCode);
+
+    Serial.print("[HTTP] begin...\n");
+    if (http.begin(client, "http://keluke.000webhostapp.com/interface.php")) // client is what type of network are u using. wifi, ethernet, gsm.
+    { 
+
+      Serial.print("[HTTP] GET...\n");
+      int httpCode = http.GET();    // start connection and send HTTP header
+      
+      if (httpCode > 0)   // httpCode will be negative on error
+      {
+        Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY)
+        {
+          payload = http.getString();
+//          Serial.println(payload);
+        }
+      }
+      else
+      {
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      }
+
+      http.end();
+    }
+    else
+    {
+      Serial.printf("[HTTP] Unable to connect\n");
+    }
+    return payload;
+  }
+  else
+  {
+    Serial.println("Unable to connect to the WiFi");
+    return "[ERROR]";
+  }
+}
